287e899f020e354d4539f31d0b59b4b1325d217a
anonymousDeveloper10 <solvgraph@hotmail.com>
2022-05-10 20:44:16 +0200

    fixes

--- app.py
+++ app.py
@@ -138,8 +138,8 @@ DEBUG = True
 app.config.from_object(__name__) """
 
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=80, debug=True)
+# if __name__ == "__main__":
+# app.run(host='0.0.0.0', port=80, debug=True)
 
 """ if __name__ == '__main__':
     app.run() """
